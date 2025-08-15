47ba9b69490babc1cc5b307e0d72d08aa6d22a34
AlanaPvart7 <alana2pvart@gmail.com>
2025-08-14 22:50:19 -0600

    Actualiza diseño del login

--- src/components/LoginScreen.jsx
+++ src/components/LoginScreen.jsx
@@ -44,7 +44,7 @@
         className="bg-cover bg-center min-h-screen flex justify-center items-center"
         style={{
             backgroundImage:
-            "url('https://i.pinimg.com/736x/b6/fa/c3/b6fac3d907a82afe8c3fb374f489c9cd.jpg')",
+            "url('https://i.imgur.com/1W0MMBa.jpeg')",
         }}
         >
         <div className="flex flex-col items-center space-y-6 bg-black/80 p-10 rounded-2xl w-[420px] shadow-xl animate-fadeIn">
@@ -59,7 +59,7 @@
             <h2 className="text-3xl font-bold text-yellow-400">
                 FuturaFree Store
             </h2>
-            <p className="text-yellow-300 text-sm italic mt-1">
+            <p className="text-yellow-300 text-sm italic">
                 El rincón del sonido eterno 🎶
             </p>
             </div>
